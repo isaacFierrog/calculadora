cda458e02a40e7e1adaec2e74c44e677e3044fe4
Isaac Fierro <isaaczid@hotmail.com>
2021-06-18 21:42:59 -0700

    Anchura dinamica de los botones y funciones nuevas añadidas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const d = document;
 
+let resultado = 0;
 const soloNumeros = dato => (dato >= "0" && dato <= "9");
 const soloUnDato = dato => (dato === 1) ? true : false;
 const mostarDatos = (selecDisplay, selecBoton) => {
@@ -8,21 +9,53 @@ const mostarDatos = (selecDisplay, selecBoton) => {
     d.addEventListener('click', e => {
         if(e.target.matches(selecBoton)){
             let $datosDisplay = $display.textContent;
-
-            if(e.target.textContent === "RESET") $display.textContent = "0";
-
-            if(e.target.textContent === "DEL" && !soloUnDato($datosDisplay.length)) 
-                $display.textContent = $datosDisplay.slice(0, $datosDisplay.length - 1);
-            else if(e.target.textContent === "DEL" && soloUnDato($datosDisplay.length))
-                $display.textContent = "0";
-        
-            if($datosDisplay === "0" && soloNumeros(e.target.textContent)) 
+            let operacionActual = "";
+            const operaciones = {
+                "+": function(){
+                    resultado += parseInt($display.textContent);
+                    $display.textContent = "0";
+                },
+                "-": function(){
+                    resultado -= parseInt($display.textContent);
+                    $display.textContent = "0";
+                },
+                "/": function(){
+                    resultado /= parseInt($display.textContent);
+                    $display.textContent = "0";
+                },
+                "x": function(){
+                    resultado *= parseInt($display.textContent);
+                    $display.textContent = "0";
+                },
+                "RESET": function(){
+                    $display.textContent = "0";
+                    resultado = 0;
+                },
+                "DEL": function(){
+                    if(!soloUnDato($datosDisplay.length)){
+                        $display.textContent = $datosDisplay
+                            .slice(0, $datosDisplay.length - 1);
+                    }else if(soloUnDato($datosDisplay.length)){
+                        $display.textContent = "0";
+                    }
+                },
+                "=": function(operacion){
+                    $display.textContent = resultado;
+                    resultado = 0;
+                }
+            }
+                
+            
+            if($datosDisplay === "0" && soloNumeros(e.target.textContent)){ 
                 $display.textContent = e.target.textContent;
-            else if(soloNumeros(e.target.textContent)) 
+            }else if(soloNumeros(e.target.textContent)){ 
                 $display.textContent += e.target.textContent;
-
+            }else{
+                let operacion = operaciones[e.target.textContent];
+                operacion();
+            }
         }
-    })
+    });
 };
 
 d.addEventListener("DOMContentLoaded", e => {
