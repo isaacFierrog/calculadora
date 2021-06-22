79448c63a6df21f0615fc8e5d90300812e0811c1
Isaac Fierro <isaaczid@hotmail.com>
2021-06-21 22:20:00 -0700

    Comportamiento de la tecla 'DEL' y '.' añadidos

--- assets/js/mostar-datos.js
+++ assets/js/mostar-datos.js
@@ -1,60 +1,71 @@
 const d = document;
 let resultado = 0;
+let operacionActual = "";
+
+
 const soloNumeros = dato => (dato >= "0" && dato <= "9");
-const soloUnDato = dato => (dato === 1) ? true : false;
+const caracteresEspeciales = dato => (dato >= 43 && dato <= 120) 
+    && !(dato >= 48 && dato <= 57);
+const caracteresOperaciones = dato => !(dato >= 48 && dato <= 57);
+const soloUnDato = dato => dato === 1;
+const operaciones = {
+    "+": function(dato){
+        resultado += parseFloat(dato.textContent);
+        operacionActual = "+";
+    },
+    "-": function(dato){
+        resultado -= parseFloat(dato.textContent);
+        operacionActual = "-";
+    },
+    "x": function(dato){
+        resultado *= parseFloat(dato.textContent);
+        operacionActual = "x";
+    },
+    "/": function(dato){
+        resultado /= parseFloat(dato.textContent);
+        operacionActual = "/";
+    },
+    "RESET": function(dato){
+        dato.textContent = "0";
+        resultado = 0;
+        operacionActual = "";
+    },
+    "=": function(dato){
+        this[operacionActual](dato);
+        dato.textContent = resultado;
+    }
+}
 
 
 const mostarDatos = (selecDisplay, selecBoton) => {
     const $display = d.querySelector(selecDisplay);
+    const teclasEspeciales = {
+        ".": (display) => {
+            if(!display.textContent.includes("."))
+                display.textContent += ".";
+        },
+        "DEL": (display) => {
+            if(!soloUnDato(display.textContent.length)){
+                const longitudDatos = display.textContent.length - 1,
+                    datosActualizados = display.textContent.slice(0, longitudDatos);
+
+                display.textContent = datosActualizados;
+            }else if(soloUnDato(display.textContent.length)){
+                display.textContent = "0";
+            }
+        }
+    };
     
-    d.addEventListener('click', e => {
+    d.addEventListener('click', e => {    
         if(e.target.matches(selecBoton)){
-            let $datosDisplay = $display.textContent;
-            let operacionActual = "";
-            const operaciones = {
-                "+": function(){
-                    resultado += parseInt($display.textContent);
-                    $display.textContent = "0";
-                },
-                "-": function(){
-                    resultado -= parseInt($display.textContent);
-                    $display.textContent = "0";
-                },
-                "/": function(){
-                    resultado /= parseInt($display.textContent);
-                    $display.textContent = "0";
-                },
-                "x": function(){
-                    resultado *= parseInt($display.textContent);
-                    $display.textContent = "0";
-                },
-                "RESET": function(){
-                    $display.textContent = "0";
-                    resultado = 0;
-                },
-                "DEL": function(){
-                    if(!soloUnDato($datosDisplay.length)){
-                        $display.textContent = $datosDisplay
-                            .slice(0, $datosDisplay.length - 1);
-                    }else if(soloUnDato($datosDisplay.length)){
-                        $display.textContent = "0";
-                    }
-                },
-                "=": function(operacion){
-                    $display.textContent = resultado;
-                    resultado = 0;
-                }
-            }
-                
-            
-            if($datosDisplay === "0" && soloNumeros(e.target.textContent)){ 
+            if(soloNumeros(e.target.textContent) && $display.textContent === "0")
                 $display.textContent = e.target.textContent;
-            }else if(soloNumeros(e.target.textContent)){ 
+            else if(soloNumeros(e.target.textContent))
                 $display.textContent += e.target.textContent;
-            }else{
-                let operacion = operaciones[e.target.textContent];
-                operacion();
-            }
+            
+            if(caracteresEspeciales(e.target.textContent.charCodeAt()))
+                teclasEspeciales[e.target.textContent]($display);
+
         }
     });
 };
